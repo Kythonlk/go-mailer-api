499281862ed48427e8cc4f0dae51ee26eeb890e2
kyhtonllk <kythonlk@gmail.com>
2024-04-29 16:33:19 +0400

    Update

--- api/hello.go
+++ api/hello.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-func main(w http.ResponseWriter, r *http.Request) {
+func Hello(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
